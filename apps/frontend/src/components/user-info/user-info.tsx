ef5d45f72734a98aa32736bafcc8c97373c18d73
gerbertwest <gerbert.west@gmail.com>
2024-03-22 20:08:33 +0300

    Выделил компонент userinfo

---
+++ apps/frontend/src/components/user-info/user-info.tsx
@@ -0,0 +1,155 @@
+import { ChangeEventHandler, MouseEventHandler } from "react"
+import { LEVELS, LOCATIONS, SEX, STATIC_DIRECTORY, UserRole } from "../../const"
+import { User } from "../../types/user"
+import { ucFirst } from "../../util"
+import SpecializationCheckbox from "../specialization-checkbox/specialization-checkbox"
+
+type UserInfoProp = {
+  role: string | undefined,
+  registryData: User,
+  avatar: File | undefined,
+  editStatus: boolean,
+  setEditStatus: React.Dispatch<React.SetStateAction<boolean>>,
+  setRegistryData: React.Dispatch<React.SetStateAction<User>>,
+  handleAvatarUpload: ChangeEventHandler<HTMLInputElement>,
+  handleSubmit: MouseEventHandler<HTMLButtonElement>,
+  handleCancel: MouseEventHandler<HTMLButtonElement>,
+  onChange: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>,
+  trainingType: string[] | undefined,
+  onChangeType: ChangeEventHandler<HTMLInputElement>,
+  readyToTraining?: boolean | undefined,
+  personalTrainings?: boolean | undefined,
+  onChangeReadyToTraining?: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>,
+  onChangePersonalTrainings?: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>,
+}
+
+function UserInfo(props: UserInfoProp): JSX.Element {
+  return (
+    <section className="user-info">
+      <div className="user-info__header">
+        <div className="input-load-avatar">
+          <label>
+            <input className="visually-hidden" type="file" name="avatar" accept="image/png, image/jpeg" onChange={props.handleAvatarUpload} disabled={!props.editStatus}/>
+              <span className="input-load-avatar__avatar">
+              {
+                  props.avatar ? (
+                      <img
+                      src={URL.createObjectURL(props.avatar)}
+                      alt="avatar"
+                      width="70" height="70" aria-hidden="true"
+                    />
+                    ) : props.registryData.avatar === '' ?
+                    (
+                      <img
+                      src="public/img/content/user-photo-1.png" srcSet="public/img/content/user-photo-1@2x.png 2x"
+                      alt="avatar"
+                      width="70" height="70" aria-hidden="true"
+                    />
+                    ) :
+                    <img
+                      src={`${STATIC_DIRECTORY}${props.registryData.avatar}`}
+                      alt="avatar"
+                      width="70" height="70" aria-hidden="true"
+                    />
+                  }
+              </span>
+          </label>
+        </div>
+        {props.editStatus === true ?
+        <div className="user-info-edit__controls">
+          <button className="user-info-edit__control-btn" aria-label="обновить" type="button" onClick={props.handleSubmit} >
+            <svg width="16" height="16" aria-hidden="true">
+              <use xlinkHref="#icon-change"></use>
+            </svg>
+          </button>
+          <button className="user-info-edit__control-btn" aria-label="удалить" onClick={props.handleCancel}>
+            <svg width="14" height="16" aria-hidden="true">
+              <use xlinkHref="#icon-trash"></use>
+            </svg>
+          </button>
+        </div> : ''}
+      </div>
+      <form className="user-info__form" action="#" method="post">
+        {props.editStatus === false ? <button className="btn-flat btn-flat--underlined user-info__edit-button" type="button" aria-label="Редактировать" onClick={() => props.setEditStatus(true)}>
+          <svg width="12" height="12" aria-hidden="true">
+            <use xlinkHref="#icon-edit"></use>
+          </svg><span>Редактировать</span>
+        </button> : ''}
+        <div className="user-info__section">
+          <h2 className="user-info__title">Обо мне</h2>
+          <div className={props.editStatus ? "custom-input user-info-edit__input" : "custom-input custom-input--readonly user-info-edit__input"}>
+            <label>
+              <span className="custom-input__label">Имя</span>
+              <span className="custom-input__wrapper">
+                <input type="text" name="name" value={props.registryData.name} disabled={!props.editStatus} onChange={props.onChange}/>
+              </span>
+            </label>
+          </div>
+          <div className="custom-textarea custom-textarea--readonly user-info__textarea">
+            <label><span className="custom-textarea__label">Описание</span>
+              <textarea name="description" placeholder=" " value={props.registryData.description}
+                onChange={props.onChange} readOnly={!props.editStatus}>
+                {props.registryData.description}
+              </textarea>
+            </label>
+          </div>
+        </div>
+        <div className="user-info__section user-info__section--status">
+          <h2 className="user-info__title user-info__title--status">Статус</h2>
+          <div className="custom-toggle custom-toggle--switch user-info__toggle">
+            {
+             props.role === UserRole.User ?
+             <label>
+              <input type="checkbox" name="readyToTraining" disabled={!props.editStatus} onChange={props.onChangeReadyToTraining} checked={props.readyToTraining}/>
+              <span className="custom-toggle__icon">
+                <svg width="9" height="6" aria-hidden="true">
+                  <use xlinkHref="#arrow-check"></use>
+                </svg>
+                </span>
+                <span className="custom-toggle__label">Готов к тренировке</span>
+              </label> :
+              <label>
+               <input type="checkbox" name="personalTrainings" disabled={!props.editStatus} onChange={props.onChangePersonalTrainings} checked={props.personalTrainings}/>
+               <span className="custom-toggle__icon">
+               <svg width="9" height="6" aria-hidden="true">
+                 <use xlinkHref="#arrow-check"></use>
+               </svg>
+               </span>
+               <span className="custom-toggle__label">Готов тренировать</span>
+             </label>
+            }
+          </div>
+        </div>
+        <div className="user-info__section">
+          <h2 className="user-info__title user-info__title--specialization">Специализация</h2>
+          <div className="specialization-checkbox user-info__specialization">
+            <SpecializationCheckbox onChangeType={props.onChangeType} trainingType={props.trainingType} editStatus={props.editStatus}/>
+          </div>
+        </div>
+        <div className="custom-select--readonly custom-select user-info__select">
+          <span className="custom-select__label">Локация</span>
+          <select className="custom-select__button" name='location' aria-label="Выберите одну из опций" value={props.registryData.location}
+            onChange={({target}) => props.setRegistryData({...props.registryData, [target.name]: target.value})} disabled={!props.editStatus}>
+                {LOCATIONS.map((local) => (<option value={local}>{local}</option>))}
+            </select>
+        </div>
+        <div className="custom-select--readonly custom-select user-info__select">
+          <span className="custom-select__label">Пол</span>
+          <select className="custom-select__button" name='sex' aria-label="Выберите одну из опций" value={props.registryData.sex}
+              onChange={({target}) => props.setRegistryData({...props.registryData, [target.name]: target.value})} disabled={!props.editStatus}>
+                {SEX.map((sex) => (<option value={sex}>{ucFirst(sex)}</option>))}
+            </select>
+        </div>
+        <div className="custom-select--readonly custom-select user-info__select">
+          <span className="custom-select__label">Уровень</span>
+          <select className="custom-select__button" name='level' aria-label="Выберите одну из опций" value={props.registryData.level}
+              onChange={({target}) => props.setRegistryData({...props.registryData, [target.name]: target.value})} disabled={!props.editStatus}>
+                {LEVELS.map((level) => (<option value={level}>{ucFirst(level)}</option>))}
+            </select>
+        </div>
+      </form>
+    </section>
+  )
+}
+
+export default UserInfo
--- apps/frontend/src/pages/coach-account-screen/coach-account-screen.tsx
+++ apps/frontend/src/pages/coach-account-screen/coach-account-screen.tsx
@@ -5,13 +5,12 @@ import { updateUserSelector, userSelector } from "../../store/user/selectors";
 import { fetchUserByIdAction, updateUser } from "../../store/api-actions";
 import { Link, useParams } from "react-router-dom";
 import LoadingScreen from "../loading-screen/loading-screen";
-import { AppRoute, LEVELS, LOCATIONS, SEX, STATIC_DIRECTORY } from "../../const";
-import { ucFirst } from "../../util";
-import SpecializationCheckbox from "../../components/specialization-checkbox/specialization-checkbox";
+import { AppRoute } from "../../const";
 import { UpdateUser } from "../../types/update-user";
 import { User } from "../../types/user";
 import CertCarousel from "../../components/cert-carousel/cert-carousel";
 import Slider from "react-slick";
+import UserInfo from "../../components/user-info/user-info";
 
 function CoachAccount(): JSX.Element {
 
@@ -185,120 +184,22 @@ function CoachAccount(): JSX.Element {
           <div className="container">
             <div className="inner-page__wrapper">
               <h1 className="visually-hidden">Личный кабинет</h1>
-              <section className="user-info-edit">
-                <div className="user-info-edit__header">
-                  <div className="input-load-avatar">
-                    <label>
-                      <input className="visually-hidden" type="file" name="avatar" accept="image/png, image/jpeg" onChange={handleAvatarUpload} disabled={!editStatus}/>
-                        <span className="input-load-avatar__avatar">
-                          {avatar ?
-                                <img
-                                src={URL.createObjectURL(avatar)}
-                                alt="avatar"
-                                width="70" height="70" aria-hidden="true"
-                              />
-                               :
-                               registryData.avatar === '' ?
-
-                                <img src="public/img/content/user-photo-1.png"
-                                srcSet="public/img/content/user-photo-1@2x.png 2x"
-                                width="98" height="98" alt="user avatar"/>
-                               :
-                                <img
-                                src={`${STATIC_DIRECTORY}${registryData.avatar}`}
-                                alt="avatar"
-                                width="70" height="70" aria-hidden="true"
-                              />
-                          }
-                         </span>
-                    </label>
-                  </div>
-
-                  {editStatus === true ?
-                  <div className="user-info-edit__controls">
-                    <button className="user-info-edit__control-btn" aria-label="обновить" type="button" onClick={handleSubmit} >
-                      <svg width="16" height="16" aria-hidden="true">
-                        <use xlinkHref="#icon-change"></use>
-                      </svg>
-                    </button>
-                    <button className="user-info-edit__control-btn" aria-label="удалить" onClick={handleCancel}>
-                      <svg width="14" height="16" aria-hidden="true">
-                        <use xlinkHref="#icon-trash"></use>
-                      </svg>
-                    </button>
-                  </div> : ''}
-
-                </div>
-                <form className="user-info-edit__form" action="#" method="post">
-                  {editStatus === false ? <button className="btn-flat btn-flat--underlined user-info__edit-button" type="button" aria-label="Редактировать" onClick={() => setEditStatus(true)}>
-                    <svg width="12" height="12" aria-hidden="true">
-                      <use xlinkHref="#icon-edit"></use>
-                    </svg><span>Редактировать</span>
-                  </button> : ''}
-
-                  <div className="user-info__section">
-                    <h2 className="user-info-edit__title">Обо мне</h2>
-                    <div className={editStatus ? "custom-input user-info-edit__input" : "custom-input custom-input--readonly user-info-edit__input"}>
-                      <label>
-                        <span className="custom-input__label">Имя</span>
-                        <span className="custom-input__wrapper">
-                          <input type="text" name="name" value={registryData.name} disabled={!editStatus} onChange={onChange}/>
-                        </span>
-                      </label>
-                    </div>
-                    <div className="custom-textarea user-info-edit__textarea">
-                      <label><span className="custom-textarea__label">Описание</span>
-                        <textarea name="description" placeholder=" " value={registryData.description}
-                        onChange={onChange} readOnly={!editStatus}>{registryData.description}
-                        </textarea>
-                      </label>
-                    </div>
-                  </div>
-                  <div className="user-info-edit__section user-info-edit__section--status">
-                    <h2 className="user-info-edit__title user-info-edit__title--status">Статус</h2>
-                    <div className="custom-toggle custom-toggle--switch user-info-edit__toggle">
-                      <label>
-                        <input type="checkbox" name="personalTrainings" disabled={!editStatus} onChange={()=> setPersonalTrainings(!personalTrainings)} checked={personalTrainings}/>
-                          <span className="custom-toggle__icon">
-                          <svg width="9" height="6" aria-hidden="true">
-                            <use xlinkHref="#arrow-check"></use>
-                          </svg>
-                          </span>
-                          <span className="custom-toggle__label">Готов тренировать</span>
-                      </label>
-                    </div>
-                  </div>
-                  <div className="user-info-edit__section">
-                    <h2 className="user-info-edit__title user-info-edit__title--specialization">Специализация</h2>
-                    <div className="specialization-checkbox user-info-edit__specialization">
-                      <SpecializationCheckbox onChangeType={onChangeType} trainingType={trainingType} editStatus={editStatus}/>
-                    </div>
-                  </div>
-                  <div className="custom-select user-info-edit__select">
-                    <span className="custom-select__label">Локация</span>
-                    <select className="custom-select__button" name='location' aria-label="Выберите одну из опций" value={registryData.location}
-                      onChange={({target}) => setRegistryData({...registryData, [target.name]: target.value})} disabled={!editStatus}>
-                          {LOCATIONS.map((local) => (<option value={local}>{local}</option>))}
-                      </select>
-                  </div>
-
-                  <div className="custom-select user-info-edit__select">
-                    <span className="custom-select__label">Пол</span>
-                     <select className="custom-select__button" name='sex' aria-label="Выберите одну из опций" value={registryData.sex}
-                       onChange={({target}) => setRegistryData({...registryData, [target.name]: target.value})} disabled={!editStatus}>
-                          {SEX.map((sex) => (<option value={sex}>{ucFirst(sex)}</option>))}
-                     </select>
-                  </div>
-
-                  <div className="custom-select user-info-edit__select">
-                    <span className="custom-select__label">Уровень</span>
-                     <select className="custom-select__button" name='level' aria-label="Выберите одну из опций" value={registryData.level}
-                       onChange={({target}) => setRegistryData({...registryData, [target.name]: target.value})} disabled={!editStatus}>
-                          {LEVELS.map((level) => (<option value={level}>{ucFirst(level)}</option>))}
-                     </select>
-                  </div>
-                </form>
-              </section>
+              <UserInfo
+                role={userData?.role}
+                registryData={registryData}
+                avatar={avatar}
+                editStatus={editStatus}
+                setEditStatus={setEditStatus}
+                setRegistryData={setRegistryData}
+                handleAvatarUpload={handleAvatarUpload}
+                handleSubmit={handleSubmit}
+                handleCancel={handleCancel}
+                onChange={onChange}
+                trainingType={trainingType}
+                onChangeType={onChangeType}
+                personalTrainings={personalTrainings}
+                onChangePersonalTrainings={() => setPersonalTrainings(!personalTrainings)}
+              />
               <div className="inner-page__content">
                 <div className="personal-account-coach">
                   <div className="personal-account-coach__navigation">
